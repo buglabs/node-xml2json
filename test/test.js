6983c8164e13503185cc8867521c7d1fdb90428f
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-06-30 15:39:36 -0400

    minor fix in tests

--- test/test.js
+++ test/test.js
@@ -3,32 +3,32 @@ var path = require('path');
 var parser = require('../lib');
 var assert = require('assert');
 
-var fixturesPath = 'fixtures';
+var fixturesPath = './fixtures';
 
 fs.readdir(fixturesPath, function(err, files) {
-    for(var i in files) {
+    for (var i in files) {
         var file = files[i];
         var ext = path.extname(file);
-               
-        if(ext == '.xml') {
+
+        if (ext == '.xml') {
             var basename = path.basename(file, '.xml');
 
             var data = fs.readFileSync(fixturesPath + '/' + file);
             var result = parser.toJson(data);
-                
+
             var jsonFile = basename + '.json'
             var expected = fs.readFileSync(fixturesPath + '/' + jsonFile) + '';
-            
-            if(result) {
+
+            if (result) {
                 result = result.trim();
             }
 
-            if(expected) {
+            if (expected) {
                 expected = expected.trim();
             }
-
             assert.deepEqual(result, expected, jsonFile + ' and ' + file + ' are different');
             console.log('All tests passed!');
         }
     }
 });
+
