8407d4568b9723ddb3f30f2c86d530f63630c545
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-07-06 13:50:16 -0400

    adds json2xml parser

--- test/test.js
+++ test/test.js
@@ -14,22 +14,35 @@ fs.readdir(fixturesPath, function(err, files) {
             var basename = path.basename(file, '.xml');
 
             var data = fs.readFileSync(fixturesPath + '/' + file);
-            var result = parser.toJson(data);
+            var result = parser.toJson(data, {reversible: true});
+
             var  data2 =  fs.readFileSync(fixturesPath + '/' + file);
             result = parser.toJson(data2);
 
-            var jsonFile = basename + '.json'
+            var jsonFile = basename + '.json';
             var expected = fs.readFileSync(fixturesPath + '/' + jsonFile) + '';
-
-            if (result) {
-                result = result.trim();
-            }
 
             if (expected) {
                 expected = expected.trim();
             }
             assert.deepEqual(result, expected, jsonFile + ' and ' + file + ' are different');
-            console.log('All tests passed!');
+            console.log('[xml2json: ' + file + '->' + jsonFile + '] passed!');
+        } else if( ext == '.json') {
+            var basename = path.basename(file, '.json');
+            if (basename.match('reversible')) {
+                var data = fs.readFileSync(fixturesPath + '/' + file);
+                var result = parser.toXml(data);
+                
+                var xmlFile = basename.split('-')[0] + '.xml';
+                var expected = fs.readFileSync(fixturesPath + '/' + xmlFile) + '';
+               
+                if (expected) {
+                    expected = expected.trim();
+                }
+                //console.log(result + '<---');
+                assert.deepEqual(result, expected, xmlFile + ' and ' + file + ' are different');
+                console.log('[json2xml: ' + file + '->' + xmlFile + '] passed!');
+            }
         }
     }
 });
