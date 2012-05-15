a10b248a2890484c721f8b5029708e1496f0d585
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-05-15 14:31:30 -0400

    wip

--- specs/spec.js
+++ specs/spec.js
@@ -1,49 +1,19 @@
 var fs = require('fs');
 var path = require('path');
-var parser = require('../lib');
-var assert = require('assert');
 
-var fixturesPath = './fixtures';
+describe('xml2json', function() {
+    it('should convert xml to json');
 
-fs.readdir(fixturesPath, function(err, files) {
-    for (var i in files) {
-        var file = files[i];
-        var ext = path.extname(file);
+    it('should convert xml to reversible json');
 
-        if (ext == '.xml') {
-            var basename = path.basename(file, '.xml');
+    it('should convert xml to a javascript object');
 
-            var data = fs.readFileSync(fixturesPath + '/' + file);
-            var result = parser.toJson(data, {reversible: true});
+    it('should escape problematic characters in element values');
+});
 
-            var  data2 =  fs.readFileSync(fixturesPath + '/' + file);
-            result = parser.toJson(data2);
+describe('json2xml', function() {
+    it('should return -1 when the value is not present');
 
-            var jsonFile = basename + '.json';
-            var expected = fs.readFileSync(fixturesPath + '/' + jsonFile) + '';
-
-            if (expected) {
-                expected = expected.trim();
-            }
-            assert.deepEqual(result, expected, jsonFile + ' and ' + file + ' are different');
-            console.log('[xml2json: ' + file + '->' + jsonFile + '] passed!');
-        } else if( ext == '.json') {
-            var basename = path.basename(file, '.json');
-            if (basename.match('reversible')) {
-                var data = fs.readFileSync(fixturesPath + '/' + file);
-                var result = parser.toXml(data);
-                
-                var xmlFile = basename.split('-')[0] + '.xml';
-                var expected = fs.readFileSync(fixturesPath + '/' + xmlFile) + '';
-               
-                if (expected) {
-                    expected = expected.trim();
-                }
-                //console.log(result + '<---');
-                assert.deepEqual(result, expected, xmlFile + ' and ' + file + ' are different');
-                console.log('[json2xml: ' + file + '->' + xmlFile + '] passed!');
-            }
-        }
-    }
+    it('should return the correct index when the value is present');
 });
 
